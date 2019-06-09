cdbfd6a1cf193596489a58bbcecbeb1fb4db40d1
github username <github email address>
2019-06-09 19:16:15 +0100

    Completed task

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,13 @@
 def count_elements(array)
   # code goes here
+  new_hash = {}
+
+  array.each do |animal|
+    !new_hash[animal].nil? ? new_hash[animal] += 1 : new_hash[animal] = 1
+  end
+
+  new_hash
 end
+
+
  
